c2267d2da83650fd613e4907bf6fd17869f30e08
Kanad4s <n.tropin@g.nsu.ru>
2025-02-11 10:29:25 +0700

    add postgres connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,11 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
     "NotesServer/internal/bd"
 )
 
 func main() {
-	a := [...]int{1, 1, 1, 2, 2, 3}
-	fmt.Println(removeDuplicates(a))
-}
-
-func removeDuplicates(nums [6]int) int {
-    bd.Connect()
-    k := 1
-    reps := 0
-    fmt.Println(nums)
-    for i := 1; i < len(nums); i++ {
-        fmt.Printf("i: %d, k: %d, reps: %d\n", i, k, reps)
-        if nums[k - 1] == nums[i] {
-            reps += 1
-            if reps < 2 {
-                k++
-            }
-        } else {
-            if reps >= 1 {
-                tmp := nums[k]
-                nums[k] = nums[i]
-                nums[i] = tmp
-                k++
-            }
-            reps = 0
-        }
-        fmt.Printf("i: %d, k: %d, reps: %d\n", i, k, reps)
-        fmt.Println(nums)
-    }
-    // fmt.Println(nums)
-    return k
+	bd.Connect()
+    // fmt.Println(bd)
 }
--- internal/bd/psql.go
+++ internal/bd/psql.go
@@ -4,21 +4,29 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 	"fmt"
+	"log"
 )
+
+//  http://84.237.50.81:8080/apex
 
 const (
-	host = "localhost"
+	host 	 = "localhost"
 	port     = 5432
-    user     = "postgres"
-    password = "your-password"
-    dbname   = "calhounio_demo"
+    user     = "kanad4s"
+    password = ""
+    dbname   = "postgres"
 )
 
+// type res struct {
+// 	id int
+//     name string
+//     salary int
+// }
+
 func Connect() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-        "password=%s dbname=%s sslmode=disable",
-        host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	dbInfo := fmt.Sprintf("host=%s port=%d dbname=%s sslmode=disable",
+        host, port, dbname)
+	db, err := sql.Open("postgres", dbInfo)
 	if err != nil {
 		panic(err)
 	}
@@ -29,5 +37,31 @@ func Connect() {
 		panic(err)
 	}
 
-	fmt.Printf("BD connected!")
+	fmt.Printf("BD connected!\n")
+	rows, ok := db.Query("select * from professions")
+	if ok != nil {
+		panic(err)
+	}
+	// fmt.Printf("response: \n%v\n", response)
+	
+	// fmt.Println(response.)
+	ids := make([]int, 0)
+	names := make([]string, 0)
+	salaries := make([]int, 0)
+    for rows.Next() {
+        var id int
+        var name string
+        var salary int
+		// var resp res
+        if err := rows.Scan(&id, &name, &salary); err != nil {
+            log.Fatal(err)
+        }
+		// fmt.Println(resp)
+        ids = append(ids, id)
+        names = append(names, name)
+        salaries = append(salaries, salary)
+    }
+	fmt.Println(ids)
+	fmt.Println(names)
+	fmt.Println(salaries)
 }
