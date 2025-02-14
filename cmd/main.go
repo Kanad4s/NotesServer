03b3f61a3e14fac64199ecb84530813bc16a1d62
Kanad4s <n.tropin@g.nsu.ru>
2025-02-15 01:46:57 +0700

    add finishing

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"NotesServer/internal/cli"
+	"NotesServer/internal/db"
 	"fmt"
-    "NotesServer/internal/db"
-    "NotesServer/internal/cli"
-    "NotesServer/internal/log"
+	// "NotesServer/internal/log"
+	"log/slog"
 )
 
 func main() {
 	db1 := db.Connect()
-    db.Request("select * from professions", db1)
-    db.Close(db1)
-    cli.ShowNavigation()
-    val, err := cli.GetNumber()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println(val)
-    log.Logger.Info("Finishing...")
+	db.Request("select * from professions", db1)
+	db.Close(db1)
+	cli.ShowNavigation()
+	val, err := cli.GetNumber()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(val)
+	slog.Info("Finishing...")
 }
